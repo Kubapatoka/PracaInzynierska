b96850fa07519d594ce64d470a8a25d8c152f058
Kubapatoka <bukaj.k@wp.pl>
2024-05-08 05:55:31 +0200

    last work majowka

--- PaszMix/paszmix/warehouse/models.py
+++ PaszMix/paszmix/warehouse/models.py
@@ -1,8 +1,16 @@
 from djmoney.models.fields import MoneyField
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=1024)
+    #params
+    #Lizyna, Metionina, Metionina+cystyna, Treonina, Tryptofan, Arginina, Walina, Izoleucyna, Białko ogólne, Wapń, Fosfor_przyswajalny, Sód, kwas_linolowy 
+
 class Delivery(models.Model):
     product_name = models.CharField(max_length=1024)
     price = MoneyField(max_digits=19, decimal_places=4, default_currency='PLN')
-    
+    date = models.DateTimeField
+    initial_quantity = models.FloatField()
+    used_quantity = models.FloatField()
+    waste = models.FloatField()
+    is_finished = models.BooleanField()
