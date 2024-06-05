979522dd5e7c45e79cce5cfc3978a4ac5483ed23
Kubapatoka <bukaj.k@wp.pl>
2024-06-05 09:50:22 +0200

    5cz2024Analizaksiazki

--- PaszMix/paszmix/warehouse/models.py
+++ PaszMix/paszmix/warehouse/models.py
@@ -1,9 +1,15 @@
 from djmoney.models.fields import MoneyField
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+#https://stackoverflow.com/questions/36477759/django-percentage-field
+PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
 
 class Product(models.Model):
     product_name = models.CharField(max_length=1024)
     #params
+    wilgotnosc = models.DecimalField(max_digits=3, decimal_places=0, default=12, validators=PERCENTAGE_VALIDATOR)
+    bialko_ogolne = 
     #Lizyna, Metionina, Metionina+cystyna, Treonina, Tryptofan, Arginina, Walina, Izoleucyna, Białko ogólne, Wapń, 
     #Fosfor_przyswajalny, Sód, kwas_linolowy 
 
